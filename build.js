be222d8f6f7be626ef14aae249f0279aae8db476
Jacob Evans <cloud887@gmail.com>
2020-03-19 23:33:31 -0500

    removed comments

--- build.js
+++ build.js
@@ -1,24 +1,21 @@
 'use strict'
 
-// Pull in our modules
 const chalk = require('chalk')
 const boxen = require('boxen')
 const fs = require('fs')
 const path = require('path')
 
-// Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
   borderStyle: 'round'
 }
 
-// Text + chalk definitions
 const data = {
   name: chalk.white('               Jacob Evans'),
   handle: chalk.white('jacobmgevans'),
   work: chalk.white('Associate Software Engineer'),
-  opensource: chalk.white('Koajs/router & Create React App') + chalk.blue('⚛').big(),
+  opensource: chalk.white('Koajs/router & Create React App') + chalk.blue('⚛'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('jacobmgevans'),
   github: chalk.gray('https://github.com/') + chalk.green('jacobmgevans'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('jacobmgevans'),
@@ -34,7 +31,6 @@ const data = {
   labelCard: chalk.white.bold('       Card:')
 }
 
-// Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
@@ -45,15 +41,14 @@ const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
-// Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
-               twittering + newline + // data.labelTwitter + data.twitter
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+const output = heading + 
+               newline + newline + 
+               working + newline + 
+               opensourcing + newline + newline + 
+               twittering + newline + 
+               githubing + newline + 
+               linkedining + newline + 
+               webing + newline + newline + 
+               carding 
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
