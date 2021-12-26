05c913939f1a93273d9a295e817340709cca3201
JacobMGEvans <jacobmgevans@gmail.com>
2021-12-26 00:49:18 -0600

    Upgrades

--- build.js
+++ build.js
@@ -1,54 +1,55 @@
-'use strict'
-
-const chalk = require('chalk')
-const boxen = require('boxen')
-const fs = require('fs')
-const path = require('path')
-
-const options = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'round'
-}
-
-const data = {
-  name: chalk.white('               Jacob Evans'),
-  handle: chalk.white('jacobmgevans'),
-  work: chalk.white('Associate Software Engineer'),
-  opensource: chalk.white('Koajs/router & Create React App') + ' ' + chalk.blueBright('⚛'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('jacobmgevans'),
-  github: chalk.gray('https://github.com/') + chalk.green('jacobmgevans'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blueBright('jacobmgevans'),
-  web: chalk.cyan('https://dev.to/jacobmgevans'),
-  npx: chalk.red('npx') + ' ' + chalk.white('@jacobmgevans/card  (via GitHub Package Registry)'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:')
-}
-
-const newline = '\n'
-const heading = `${data.name} / ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
-
-const output = heading + 
-               newline + newline + 
-               working + newline + 
-               opensourcing + newline + newline + 
-               twittering + newline + 
-               githubing + newline + 
-               linkedining + newline + 
-               webing + newline + newline + 
-               carding 
-
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+"use strict";
+exports.__esModule = true;
+var chalk_1 = require("chalk");
+var boxen_1 = require("boxen");
+var fs_1 = require("fs");
+var path_1 = require("path");
+var data = {
+    name: chalk_1["default"].white("               Jacob Evans"),
+    handle: chalk_1["default"].white("jacobmgevans"),
+    work: chalk_1["default"].white("Associate Software Engineer"),
+    opensource: chalk_1["default"].white("Koajs/router & Create React App") + " " + chalk_1["default"].blue("⚛"),
+    twitter: chalk_1["default"].gray("https://twitter.com/") + chalk_1["default"].cyan("jacobmgevans"),
+    github: chalk_1["default"].gray("https://github.com/") + chalk_1["default"].green("jacobmgevans"),
+    linkedin: chalk_1["default"].gray("https://linkedin.com/in/") + chalk_1["default"].blue("jacobmgevans"),
+    web: chalk_1["default"].cyan("https://dev.to/jacobmgevans"),
+    npx: chalk_1["default"].red("npx") +
+        " " +
+        chalk_1["default"].white("@jacobmgevans/card  (via GitHub Package Registry)"),
+    labelWork: chalk_1["default"].white.bold("       Work:"),
+    labelOpenSource: chalk_1["default"].white.bold("Open Source:"),
+    labelTwitter: chalk_1["default"].white.bold("    Twitter:"),
+    labelnpm: chalk_1["default"].white.bold("        npm:"),
+    labelGitHub: chalk_1["default"].white.bold("     GitHub:"),
+    labelLinkedIn: chalk_1["default"].white.bold("   LinkedIn:"),
+    labelWeb: chalk_1["default"].white.bold("        Web:"),
+    labelCard: chalk_1["default"].white.bold("       Card:")
+};
+var newline = "\n";
+var heading = "".concat(data.name, " / ").concat(data.handle);
+var working = "".concat(data.labelWork, "  ").concat(data.work);
+var opensourcing = "".concat(data.labelOpenSource, "  ").concat(data.opensource);
+var twittering = "".concat(data.labelTwitter, "  ").concat(data.twitter);
+var githubing = "".concat(data.labelGitHub, "  ").concat(data.github);
+var linkedining = "".concat(data.labelLinkedIn, "  ").concat(data.linkedin);
+var webing = "".concat(data.labelWeb, "  ").concat(data.web);
+var carding = "".concat(data.labelCard, "  ").concat(data.npx);
+var output = heading +
+    newline +
+    newline +
+    working +
+    newline +
+    opensourcing +
+    newline +
+    newline +
+    twittering +
+    newline +
+    githubing +
+    newline +
+    linkedining +
+    newline +
+    webing +
+    newline +
+    newline +
+    carding;
+(0, fs_1.writeFileSync)((0, path_1.join)(__dirname, "bin/output"), chalk_1["default"].green((0, boxen_1["default"])(output)));
---
+++ build.ts
@@ -0,0 +1,62 @@
+"use strict";
+
+import chalk, { Options } from "chalk";
+import boxen from "boxen";
+import { writeFileSync } from "fs";
+import { join } from "path";
+
+const data = {
+  name: chalk.white("               Jacob Evans"),
+  handle: chalk.white("jacobmgevans"),
+  work: chalk.white("Associate Software Engineer"),
+  opensource:
+    chalk.white("Koajs/router & Create React App") + " " + chalk.blue("⚛"),
+  twitter: chalk.gray("https://twitter.com/") + chalk.cyan("jacobmgevans"),
+  github: chalk.gray("https://github.com/") + chalk.green("jacobmgevans"),
+  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("jacobmgevans"),
+  web: chalk.cyan("https://dev.to/jacobmgevans"),
+  npx:
+    chalk.red("npx") +
+    " " +
+    chalk.white("@jacobmgevans/card  (via GitHub Package Registry)"),
+  labelWork: chalk.white.bold("       Work:"),
+  labelOpenSource: chalk.white.bold("Open Source:"),
+  labelTwitter: chalk.white.bold("    Twitter:"),
+  labelnpm: chalk.white.bold("        npm:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelWeb: chalk.white.bold("        Web:"),
+  labelCard: chalk.white.bold("       Card:"),
+};
+
+const newline = "\n";
+const heading = `${data.name} / ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const opensourcing = `${data.labelOpenSource}  ${data.opensource}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
+
+const output =
+  heading +
+  newline +
+  newline +
+  working +
+  newline +
+  opensourcing +
+  newline +
+  newline +
+  twittering +
+  newline +
+  githubing +
+  newline +
+  linkedining +
+  newline +
+  webing +
+  newline +
+  newline +
+  carding;
+
+writeFileSync(join(__dirname, "bin/output"), chalk.green(boxen(output)));
