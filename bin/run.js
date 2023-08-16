700ea4b9a55acec27ab13439fa6e2247e224f44e
Jacob M-G Evans <jacobmgevans@gmail.com>
2023-08-16 15:19:33 -0500

    remove unused

--- bin/run.js
+++ bin/run.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { execSync, spawnSync } from "node:child_process";
-import { resolve, dirname, join } from "node:path";
+import { execSync } from "node:child_process";
+import { join } from "node:path";
 
 console.log(
   execSync(`node ${join(process.cwd(), "dist/index.js")}`).toString()
